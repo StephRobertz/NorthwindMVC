78e0b183ca96ad74fce40931f65f0675454c6dcd
steph <steph@STEFI_GALAXY>
2022-12-20 13:43:30 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindMVC.Models;
+
+namespace NorthwindMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private NorthwindOriginalEntities db = new NorthwindOriginalEntities();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else { 
+            var products = db.Products.Include(p => p.Categories).Include(p => p.Suppliers);
+            return View(products.ToList());
+            }
+        }
+
+        public ActionResult Index2()
+        {
+           
+        var products = db.Products.Include(p => p.Categories).Include(p => p.Suppliers);
+            return View(products.ToList());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(products);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            return View(products);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            return View(products);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued,ImageLink")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            return View(products);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Products products = db.Products.Find(id);
+            db.Products.Remove(products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
